21fe490df6d8d1fbb9fbbe5834b757185142fdfe
sammisam8888 <samuelpriyatam@gmail.com>
2025-01-14 19:38:16 +0530

    created-counter-app

---
+++ counter-project/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+ let [counter,setcounter] = useState(0)
+  //usestate is a react hook
+  //here setcounter is not a default value, it can be any name, but for convinience here we use set counter for counter variable.. in the background this entire thing behaves like 'let counter = 0'
+
+  // let counter=0; //- not required as state is used to set the value of counter - here the problem was the value wasnt being updated on the actual frontend, it was just updated in console log 
+  const Increment= () => {
+    setcounter(counter+1)
+    console.log("value incremented : ", counter)
+  }
+  const Decrement= () => {
+    setcounter(counter-1)
+    console.log("value decremented : ", counter)
+  }
+  const Reset= () => {
+    setcounter(0)
+    console.log("value reseted : ", counter)
+  }
+
+  return (
+    <>
+      <h1>Counter App</h1>
+      <h2>Counter value : {counter}</h2>
+      <button onClick={Increment}>Increase value</button>
+      <br />
+      <button onClick={Decrement}>Decrease value</button>
+      <br />
+      <button onClick={Reset}>Reset value</button>
+    </>
+  )
+}
+
+export default App
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { createRoot } from 'react-dom/client'
 import React from 'react'
 import App from './App.jsx'
+import {jsx as _jsx} from 'react/jsx-runtime'
 
 function App2(){
   return(
@@ -12,17 +13,17 @@ function App2(){
   )
 }
 
-// createRoot(document.getElementById('root')).render(
-//     <App />
-// )
-const company = "ganesh"
+createRoot(document.getElementById('root')).render(
+    <App />
+)
+const company = "google"
 const link = "https://"+company+".com"
 const ReactElement = React.createElement(
     'a',  //first parameter - tag name
   {
   href : link, target : '_blank', //2nd properties of the tag given above
   },
-  'Click to Visit ', //3rd - here it defines the text to be displayed on the anchor tag
+  'Click to Visit ', //3rd parameter - children - here it defines the text to be displayed on the anchor tag
   company 
 )
 
@@ -33,7 +34,7 @@ const anotherElement = (
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // App2()  //will also call the function - javascript syntax for execution
+  // App2(),  //will also call the function - javascript syntax for execution
   //<App2 /> //is another way to call the function for rendering in javascript
 
   
