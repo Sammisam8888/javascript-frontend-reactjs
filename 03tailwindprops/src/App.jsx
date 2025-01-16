f5b8f2f86acfbbf99d91da5abb5ecee60be34fc9
sammisam8888 <samuelpriyatam@gmail.com>
2025-01-16 21:07:52 +0530

    added-tailwind-css-props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
+  let myobj={
+    username:"samuel",
+    age:19
+  }
 
   return (
     <>
-      <h1 className='bg-red-400 text-black p-4 rounded-xl'>TAILWIND test</h1>
-      
+      {/* <h1 className='bg-red-400 text-black p-4 rounded-xl'></h1> */}
+      <Card username="Sammisam" btnText="Click Me"/>
+      <Card username="Trish" btnText = "Follow me"/>
+
     </>
   )
 }
